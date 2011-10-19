0fb025469f25012eaee41f46c7c6430c940dee78
Tom Stuart <tom@experthuman.com>
2011-10-19 07:48:57 +0100

    Add empty module for implementations

---
+++ lib/nothing.rb
@@ -0,0 +1,71 @@
+module Nothing
+  # Natural numbers
+
+  # ZERO  =
+  # ONE   =
+  # TWO   =
+  # THREE =
+
+  # TIMES     =
+  # INCREMENT =
+  # ADD       =
+  # MULTIPLY  =
+  # POWER     =
+  # DECREMENT =
+  # SUBTRACT  =
+
+  # Booleans
+
+  # TRUE  =
+  # FALSE =
+  # IF    =
+
+  # NOT =
+  # AND =
+  # OR  =
+
+  # Natural numbers with booleans
+
+  # IS_ZERO           =
+  # IS_LESS_OR_EQUAL  =
+  # IS_EQUAL          =
+
+  # Natural numbers with recursion
+
+  # FACTORIAL =
+  # DIV       =
+  # MOD       =
+
+  # Pairs
+
+  # PAIR  =
+  # LEFT  =
+  # RIGHT =
+
+  # Lists
+
+  # EMPTY     =
+  # UNSHIFT   =
+  # IS_EMPTY  =
+  # FIRST     =
+  # REST      =
+
+  # RANGE   =
+  # SUM     =
+  # PRODUCT =
+  # CONCAT  =
+  # PUSH    =
+  # REVERSE =
+
+  # INCREMENT_ALL =
+  # DOUBLE_ALL    =
+
+  # Natural numbers with lists
+
+  # TO_DIGITS =
+  # TO_STRING =
+
+  # FizzBuzz
+
+  # FIZZBUZZ =
+end
